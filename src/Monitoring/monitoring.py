0a1da0177bd316ceaf44403693edca4d0d5af7ba
drrben <benjammin.derre@polytechnique.edu>
2021-10-20 13:23:43 +0200

    add psi

--- src/Monitoring/monitoring.py
+++ src/Monitoring/monitoring.py
@@ -43,6 +43,40 @@ def check_nb_nan (data_1, data_2):
     return diff_in_percent> 0.01
 
 
+def psi(data_1, data_2, buckettype='bins', buckets=10):
+    def scale_range(input, min, max):
+        input += -(np.min(input))
+        input /= np.max(input) / (max - min)
+        input += min
+        return input
+
+    breakpoints = np.arange(0, buckets + 1) / (buckets) * 100
+
+    if buckettype == 'bins':
+        breakpoints = scale_range(breakpoints, np.min(data_1), np.max(data_1))
+    elif buckettype == 'quantiles':
+        breakpoints = np.stack([np.percentile(data_1, b) for b in breakpoints])
+
+    expected_percents = np.histogram(data_1, breakpoints)[0] / len(data_1)
+    actual_percents = np.histogram(data_2, breakpoints)[0] / len(data_2)
+
+    def sub_psi(e_perc, a_perc):
+        '''Calculate the actual PSI value from comparing the values.
+            Update the actual value to a very small number if equal to zero
+        '''
+        if a_perc == 0:
+            a_perc = 0.0001
+        if e_perc == 0:
+            e_perc = 0.0001
+
+        value = (e_perc - a_perc) * np.log(e_perc / a_perc)
+        return (value)
+
+    psi_value = np.sum(sub_psi(expected_percents[i], actual_percents[i]) for i in range(0, len(expected_percents)))
+
+    return psi_value>0.5
+
+
 def apply_test(df, batch, test):
     int_col = u.get_numerical_columns(df)
     results = {}
@@ -67,7 +101,7 @@ def main_monitoring(df,batches):
 
 def main_monitoring_batch(df,batch):
     check_set_columns(df,batch)
-    tests = ["check_nb_nan","kolmogorov_smirnov", "t_test", "levene_test"]
+    tests = ["check_nb_nan","kolmogorov_smirnov", "t_test", "levene_test","psi"]
     for test in tests:
         function_test= globals()[test]
         apply_test(df, batch, function_test)
