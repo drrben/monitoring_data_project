c20bd759412f0482eca1faea66949a94be455763
mbatoul <batoulmathis@gmail.com>
2021-10-21 12:13:46 +0200

    Use relative import for Loading module in viz file.

--- src/Visualization/visualization.py
+++ src/Visualization/visualization.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
-import sys
-sys.path.insert(0,"../Loading/")
-import loading
+from ..Loading.loading import read_csv_from_path, read_csv_from_path
 logger = logging.getLogger('main_logger')
 import streamlit as st
 import pandas as pd
@@ -10,16 +8,14 @@ import numpy as np
 import plotly.figure_factory as ff
 
 st.title('Monitoring Dashboard')
-df= loading.read_csv_from_path("../../Outputs/Preprocessed/marketing_preprocessed.csv")
-cols=df.columns
+
+df = read_csv_from_path("../../Outputs/Preprocessed/marketing_preprocessed.csv")
+cols = df.columns
 option = st.selectbox('feature',cols)
-train=df[option]
-df2= loading.read_csv_from_path("../../Outputs/Preprocessed/marketing_2_preprocessed.csv")[option]
-data=np.array_split(df2,4)+[train]
+train = df[option]
+
+df2 = read_csv_from_path("../../Outputs/Preprocessed/marketing_2_preprocessed.csv")[option]
+data = np.array_split(df2,4)+[train]
+
 fig = ff.create_distplot(data,["batch_1","batch_2","batch_3","batch_4","train"])
 st.plotly_chart(fig, use_container_width=True)
-
-
-
-
-
